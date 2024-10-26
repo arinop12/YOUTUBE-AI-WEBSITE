c8acac0e732346104091693dc5eaa7c050fc4522
niraop <niralibert@gmail.com>
2024-10-26 12:47:34 +0530

    1st Commit
    AI generate website using nodeJs

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs',{
+    
+    })
+})
+
+app.listen(PORT, () =>{
+    console.log("Litening to the server");
+})
